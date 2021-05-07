e9df0181f92a263894dd6de1dbe6a64a821cc070
Abdon Ribeiro <abdonribeiro10@gmail.com>
2021-05-07 19:21:38 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerReposytory.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+public interface SellerReposytory extends JpaRepository<Seller, Long>{
+	
+	
+	
+
+}
